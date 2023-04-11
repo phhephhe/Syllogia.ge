80a332fe3197be1c1ac8f98e90a7be515f02a42b
Davit Beridze <beridzedavit07@gmail.com>
2023-04-10 19:22:34 -0700

    added team component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainSlideComponent } from './main-slide/main-slide.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { MainProjectComponent } from './main-project/main-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainSlideComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    MainProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-project/main-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-project',
+  templateUrl: './main-project.component.html',
+  styleUrls: ['./main-project.component.css']
+})
+export class MainProjectComponent {
+
+}
